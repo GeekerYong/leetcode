99b7b98da1c6bc14e88c6a7bc7289d7ee6246fae
Geeker Yong <451010444@qq.com>
2018-06-06 22:45:05 +0800

    202. 快乐数 未完成

---
+++ src/happyNumber.java
@@ -0,0 +1,13 @@
+/**
+ * Created by sw on 2018-06-06.
+ */
+public class happyNumber {
+    static public boolean isHappy(int n) {
+        while (n != 1 && n != 4) {
+            int ans = 0;
+            while (n) {
+                ans + = n
+            }
+        }
+    }
+}
