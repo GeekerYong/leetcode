4cef30d5bedbe9b28d8deba15de2d566d59dcc33
khazix <451010444@qq.com>
2018-06-07 10:24:41 +0800

    202. 肥宅快乐数 完成

--- src/happyNumber.java
+++ src/happyNumber.java
@@ -5,9 +5,26 @@ public class happyNumber {
     static public boolean isHappy(int n) {
         while (n != 1 && n != 4) {
             int ans = 0;
-            while (n) {
-                ans + = n
+            boolean flag = false;
+            while (!flag) {
+                ans += (n % 10) * (n % 10);
+                n = n / 10;
+                if (n == 0) {
+                    flag = true;
+                }
             }
+            n = ans;
         }
+
+        if (n == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 19;
+        System.out.println(isHappy(n));
     }
 }
