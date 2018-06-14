3ceb3e8163c5ae4c144f82281c1e78e5b876a3af
Geeker Yong <451010444@qq.com>
2018-06-14 15:58:36 +0800

    342. 4的幂

---
+++ src/powerofFour.java
@@ -0,0 +1,31 @@
+/**
+ * Created by sw on 2018-06-14.
+ */
+public class powerofFour {
+    static boolean isPowerOfFour(int num) {
+        //循环
+        if (num <= 0) return false;
+        while (num > 1) {
+            if (num % 4 != 0) {
+                return false;
+            }
+            num = num / 4;
+        }
+        return true;
+    }
+
+    static boolean isPowerOfFour2(int num) {
+        //递归
+        if (num <= 0) return false;
+        if (num == 1) return true;
+        return (num % 4 == 0) && isPowerOfFour2(num / 4);
+    }
+
+    static boolean isPowerOfFour3(int num) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfFour(16));
+    }
+}
