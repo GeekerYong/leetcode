175c3848b17526b53c164ed6dd887abcf0b30453
Geeker Yong <451010444@qq.com>
2018-06-14 10:58:59 +0800

    290. 单词模式 fix bug

--- src/wordPattern.java
+++ src/wordPattern.java
@@ -15,20 +15,24 @@ public class wordPattern {
         } // 长度不同，必定不是相同模式
 
         for (int i = 0; i < strPat.length; i++) {
-            if (rela1.containsKey(strPat[i]) || rela2.containsKey(strPat[i])) {
+            if (rela1.containsKey(strPat[i]) || rela2.containsKey(strArr[i])) {
+                boolean flag1 = false;
+                boolean flag2 = false;
                 if (rela1.containsKey(strPat[i])) {
-                    if (rela1.get(strPat[i]) != strArr[i]) return false;
-                } else {
-                    if (rela2.get(strArr[i]) != strPat[i]) return false;
+                    String s1 = rela1.get(strPat[i]);
+                    flag1 = s1.equals(strArr[i]);
                 }
+                if (rela2.containsKey(strArr[i])) {
+                    char c1 = rela2.get(strArr[i]);
+                    flag2 = c1 == strPat[i];
+                }
+                if (!flag1 || !flag2) return false;
             } else {
                 rela1.put(strPat[i], strArr[i]);
                 rela2.put(strArr[i], strPat[i]);
             }
         }
-
         return true;
-
     }
 
     public static void main(String[] args) {
