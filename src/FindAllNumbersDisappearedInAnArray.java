f3a971d8fd6ae7a561f798be56141340ec350d53
Geeker Yong <451010444@qq.com>
2018-06-29 11:47:54 +0800

    448. 找到所有数组中消失的数字

---
+++ src/FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by sw on 2018-06-29.
+ */
+public class FindAllNumbersDisappearedInAnArray {
+
+    static public List<Integer> solution(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        boolean[] bucket = new boolean[nums.length];
+        for (int i = 0; i < bucket.length; i++) {
+            bucket[i] = false;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            bucket[nums[i]] = true;
+        }
+
+        for (int i = 0; i < bucket.length; i++) {
+            if (bucket[i] == false) {
+                ans.add(i);
+            }
+        }
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
